e7b96bb01e90512f9ceb82254d9014313a72e542
Reo YONEYAMA <yoneyama.reo@g.sp.m.is.nagoya-u.ac.jp>
2021-05-13 14:50:03 +0900

    Update __init__.py

--- usfgan/layers/__init__.py
+++ usfgan/layers/__init__.py
@@ -1,5 +1,4 @@
 from usfgan.layers.residual_block import *  # NOQA
 from usfgan.layers.upsample import *        # NOQA
-from usfgan.layers.residual_block import *  # NOQA
 from usfgan.layers.source_network import *  # NOQA
 from usfgan.layers.filter_network import *  # NOQA
